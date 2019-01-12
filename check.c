cc321342886a78079d5b8b6ac21a71c8041cf44f
FBeaumont <flbeaumo@student.42.fr>
2019-01-12 15:06:17 +0100

    checking work?

--- check.c
+++ check.c
@@ -5,55 +5,81 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: flbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/09 16:45:56 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/01/12 03:23:54 by florian          ###   ########.fr       */
+/*   Created: 2019/01/12 14:49:11 by flbeaumo          #+#    #+#             */
+/*   Updated: 2019/01/12 15:05:52 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int	check_line(char *buf)
+static int	check_block(char *buf)
 {
-    int i;
-    int dot;
-    int sharp;
-    int cl;
+	int i;
+	int dot;
+	int sharp;
+	int line;
 
-    cl = 0;
-    dot = 0;
-    sharp = 0;
-    i = 0;
-    while (i < 20)
-    {
-        if (buf[i] == '\n')
-            cl++;
-        if (buf[i] == '.')
-            dot++;
-        if (buf[i] == '#')
-            sharp++;
-        i++;
-    }
-
-    if (dot != 12 || sharp != 4 || cl != 4)
-        return (0);
-    if (buf[20] == '\n')
-        cl++;
-    return (cl);
+	i = 0;
+	dot = 0;
+	sharp = 0;
+	line = 0;
+	while (i < 20)
+	{
+		if (buf[i] == '\n')
+			line++;
+		if (buf[i] == '.')
+			dot++;
+		if (buf[i] == '#')
+			sharp++;
+		i++;
+	}
+	if (dot != 12 || sharp != 4 || line != 4)
+		return (0);
+	if (buf[20] == '\n')
+		line++;
+	return (line);
 }
 
-int			read_file(char *av)
+int			check_tetri(char *buf)
 {
-    int fd;
-    int ret;
-    char buf[21];
+	int i;
+	int count;
 
-    fd = open(av, O_RDONLY);
-    while ((ret = read(fd, buf, 21)))
-    {
-        buf[ret] = '\0';
-        if (!check_line(buf))
-            return (0);
-    }
-//    close(fd);
-    return (1);
+	i = 0;
+	count = 0;
+	while (i < 20)
+	{
+		if (buf[i] == '#')
+		{
+			if ((i + 1) < 20 && buf[i + 1] == '#')
+				count++;
+			if ((i - 1) >= 0 && buf[i - 1] == '#')
+				count++;
+			if ((i + 5) < 20 && buf[i + 5] == '#')
+				count++;
+			if ((i - 5) >= 0 && buf[i - 5] == '#')
+				count++;
+		}
+		i++;
+	}
+	return (count == 6 || count == 8 ? count : 0);
+}
+
+int			input(char *av)
+{
+	int		fd;
+	int		ret;
+	char	buf[21];
+
+	fd = open(av, O_RDONLY);
+	while ((ret = read(fd, buf, 21)))
+	{
+		buf[ret] = '\0';
+		if (!check_block(buf))
+			return (0);
+		if (!check_tetri(buf))
+			return (0);
+	}
+	close(fd);
+	return (1);
 }
