8b6625169de12c3f62529b6c67dd427b290b76b6
FBeaumont <flbeaumo@student.42.fr>
2019-01-12 19:41:15 +0100

    update

--- func.c
+++ func.c
@@ -1,39 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   func.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/09 16:43:53 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/01/12 19:17:37 by flbeaumo         ###   ########.fr       */
+/*   Created: 2019/01/12 19:15:25 by flbeaumo          #+#    #+#             */
+/*   Updated: 2019/01/12 19:19:36 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
+#include "fillit.h"
 
-# include <fcntl.h>
-# include "libft/includes/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef struct	s_point
+t_tri	clean_lst(t_tri lst)
 {
-	int	x;
-	int	y;
-}				t_point;
-
-typedef struct 	s_tri
-{
-	char c;
-	t_point pos[4];
-	struct s_tri *next;
-}				t_tri;
-
-t_tri	*parse(char *av);
-t_tri	clean_lst(t_tri lst);
-int		input(char *av);
+	while (lst != NULL)
+	{
+		ft_strdel(lst);
+		lst = lst->next;
+	}
+	return (lst);
+}
 
 
-#endif
--- map.c
+++ map.c
@@ -1,39 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/09 16:43:53 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/01/12 19:17:37 by flbeaumo         ###   ########.fr       */
+/*   Created: 2019/01/12 19:15:10 by flbeaumo          #+#    #+#             */
+/*   Updated: 2019/01/12 19:15:17 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
 
-# include <fcntl.h>
-# include "libft/includes/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef struct	s_point
-{
-	int	x;
-	int	y;
-}				t_point;
-
-typedef struct 	s_tri
-{
-	char c;
-	t_point pos[4];
-	struct s_tri *next;
-}				t_tri;
-
-t_tri	*parse(char *av);
-t_tri	clean_lst(t_tri lst);
-int		input(char *av);
-
-
-#endif
